bd57a579edb7ceba7354a9fa33dd7f1aa21c7eb8
SharkSnack <sharksnack2@gmail.com>
2020-12-08 10:22:04 -0600

    finished tower of fortune guide

--- shiren5/scripts/common.js
+++ shiren5/scripts/common.js
@@ -7,4 +7,8 @@ $(document).ready(function() {
   } catch(e) {
     console.log('error: ' + e);
   }
+  // Toggle boss content on dungeon pages
+  $("#boss_button").click(function() { 
+      $("#boss_content").toggle();
+  });
 });
