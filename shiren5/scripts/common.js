042b1aad9736be56b37e9debd06ba45bc57bfebf
SharkSnack <sharksnack2@gmail.com>
2021-02-15 03:54:41 -0600

    we don't need jQuery mate

--- shiren5/scripts/common.js
+++ shiren5/scripts/common.js
@@ -1,15 +1,19 @@
-$(document).ready(function() {
-  // load some common HTML
-  try {
-    $("#sidebar").load("../sidebar.html", function() {
-      console.log('info - loaded sidebar');
-    });
-  } catch(e) {
-    console.log('error: ' + e);
-  }
-  // Toggle spoiler content
-  $("#spoiler_button").click(function() { 
-      $("#spoiler_content").toggle();
+// load sidebar
+fetch('../sidebar.html')
+  .then(response => response.text())
+  .then(value => {
+    document.getElementById('sidebar').innerHTML = value
   });
 
-});
+document.addEventListener('DOMContentLoaded', function() {
+
+  // toggle spoiler content on spoiler button click
+  let spoiler_button = document.querySelector('#spoiler_button');
+
+  if (spoiler_button !== null) {
+    spoiler_button.addEventListener("click", e => {
+      document.querySelector('#spoiler_content').classList.toggle('hide')
+    });
+  }
+
+}, false);
